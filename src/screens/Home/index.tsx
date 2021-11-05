1dac3cb33a72f86f3ba1ec4861d0cadf2c7bbf1f
Vitor Nunes <stecks10@gmail.com>
2021-11-05 10:09:04 -0300

    Criação do Card Car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,9 +4,33 @@ import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
 
+import { Car } from '../../components/Car';
+
 import { Container, Header, TotalCars, HeaderContent } from './styles';
 
 export function Home() {
+  const carDataOne = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'AO DIA',
+      price: 120,
+    },
+    thumbnail:
+      'https://www.motortrend.com/uploads/sites/10/2018/05/2018-audi-rs5-4wd-coupe-angular-front.png',
+  };
+
+  const carDataTwo = {
+    brand: 'PORSHE',
+    name: 'PANAMERA',
+    rent: {
+      period: 'AO DIA',
+      price: 120,
+    },
+    thumbnail:
+      'https://www.webmotors.com.br/imagens/prod/347468/PORSCHE_PANAMERA_2.9_V6_EHYBRID_4_PDK_3474681900348621.png?s=fill&w=130&h=97&q=70&t=true)',
+  };
+
   return (
     <Container>
       <StatusBar
@@ -20,6 +44,9 @@ export function Home() {
           <TotalCars>total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carDataOne} />
+      <Car data={carDataTwo} />
     </Container>
   );
 }
