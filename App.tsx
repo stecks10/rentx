8eff07b01aa5cc0e648252f9e710582efa990826
Vitor Nunes <stecks10@gmail.com>
2021-11-04 21:19:29 -0300

    criando projeto

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter';
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from '@expo-google-fonts/archivo';
+
+import { Home } from './src/screens/Home';
+
+export default function App() {
+  return <Home />;
+}
