75c8e7738f3af9bf54e8015f14ec2ad0ce11d7ec
Vitor Nunes <stecks10@gmail.com>
2021-11-05 19:46:27 -0300

    Componente de Slider

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
-import { Container, Header } from './style';
+import { Container, Header, CarImage } from './style';
 
 export function CarDetails() {
   return (
@@ -9,6 +10,14 @@ export function CarDetails() {
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
+
+      <CarImage>
+        <ImageSlider
+          imagesUrl={[
+            'https://www.motortrend.com/uploads/sites/10/2018/05/2018-audi-rs5-4wd-coupe-angular-front.png',
+          ]}
+        />
+      </CarImage>
     </Container>
   );
 }
